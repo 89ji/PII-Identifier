7fdcaf4af80c9d04a1d97d4f83a3ff9e624f922e
KaiCastell <kaieecastellanos@gmail.com>
2025-03-09 17:15:16 -0500

    Added files for DOB and SSN
    Description

---
+++ Regexs/dob.py
@@ -0,0 +1,26 @@
+import re
+
+def removeDOB(text):
+    returnPhrase = ""
+    lines = text.split("\n")
+    lineNum = 0
+
+    for x in lines:
+        # the find regex patterns 
+        dobPhrases = [r"\bdate[\s\-_]*of[\s\-_]*birth\b", r"D[\s\-_]O[\s\-_]B"]
+        datePhrases = [r"\b\d{4}[\s\-/]*\d{1,2}[\s\-/]*\d{1,2}\b", r"\b\d{1,2}[/\-]\d{1,2}[/\-]\d{4}\b"]
+
+        # DOB section
+        for y in dobPhrases:
+            if re.search(y, x, re.IGNORECASE):
+                print(f"Match found on line {lineNum}: DOB phrase detected.")
+                
+                # replacement time
+                for z in datePhrases:
+                    x = re.sub(z, "*dob*", x, flags=re.IGNORECASE)
+
+        # store line, also any modifications
+        returnPhrase += x + "\n"
+        lineNum += 1  # counting lines
+
+    return returnPhrase
