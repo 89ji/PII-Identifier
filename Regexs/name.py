5a38dd94a78eaa778f5b09a46b664ecd539c2624
RonanRao <164988959+RonanRao@users.noreply.github.com>
2025-03-09 18:21:39 -0500

    Update name.py

--- Regexs/name.py
+++ Regexs/name.py
@@ -5,10 +5,20 @@ nlp = spacy.load("en_core_web_sm")
 
 name = r"[A-Z][a-z]+ [A-Z][a-z]+"
 with open("ehr JMS.txt", "r") as file:
-
+    
+    redacted = []
     for line in file:   
         result = re.findall(name, line) # Find potenial first and last names
         for match in result:
             doc = nlp(match) # Process possible names through spaCy
             if any(ent.label_ == "PERSON" for ent in doc.ents):
-                print(match)
+               #print(match)
+               line = line.replace(match, "*name*")  # Replace name
+        redacted.append(line)
+
+# Test with file output
+#with open("ehr_JMS_redacted.txt", "w") as output_file:
+#    output_file.writelines(redacted)
+
+#print("Redacted file saved as 'ehr_JMS_redacted.txt'")
+     
