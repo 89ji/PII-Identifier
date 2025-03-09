fb86b271bcdf6ac6505d5fcb8b2349cf31b7bad3
KaiCastell <kaieecastellanos@gmail.com>
2025-03-09 15:39:26 -0500

    fixed some things
    better comments and removed a useless variable

--- dob-ssn.py
+++ dob-ssn.py
@@ -8,30 +8,29 @@ def removePII():
         lineNum = 0
 
         for x in lines:
-            found = False
-            # Patterns
+            # the find regex patterns 
             dobPhrases = [r"\bdate[\s\-_]*of[\s\-_]*birth\b", r"D[\s\-_]O[\s\-_]B"]
             datePhrases = [r"\b\d{4}[\s\-/]*\d{1,2}[\s\-/]*\d{1,2}\b", r"\b\d{1,2}[/\-]\d{1,2}[/\-]\d{4}\b"]
             ssnPhrases = [r"\d{3}[\s\-_]?\d{2}[\s\-_]?\d{4}"]
 
-            # Search and replace DOB indicators
+            # DOB section
             for y in dobPhrases:
                 if re.search(y, x, re.IGNORECASE):
                     print(f"Match found on line {lineNum}: DOB phrase detected.")
                     
-                    # Replace date mentions in the same line
+                    # replacement time
                     for z in datePhrases:
                         x = re.sub(z, "*dob*", x, flags=re.IGNORECASE)
             
-            # Search and replace SSNs
+            # SSN section
             for y in ssnPhrases:
                 x = re.sub(y, "*ssn*", x, flags=re.IGNORECASE)
 
-            # Store modified line
+            # store line, also any modifications
             returnPhrase += x
-            lineNum += 1  # Move to next line
+            lineNum += 1  # counting lines
 
     return returnPhrase
 
-# Call function and print cleaned text
+# call function, the print/return is the new file
 print(removePII())
