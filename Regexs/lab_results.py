026481132532c70c76cba5cd1becb9d6e3c5ea43
Justin Lai <justin88.lai@gmail.com>
2025-03-28 11:07:31 -0500

    Fixed allergies and lab_results

--- Regexs/lab_results.py
+++ Regexs/lab_results.py
@@ -9,14 +9,14 @@ def removeLabResults(text):
     for line in lines:
 
         # ignore bulleted sections please
-        if inside_lab_section and not re.match(r"^\s*[-•]", line):
+        if inside_lab_section and not re.match(r"(^\s*$|^\s*[-•])", line):
             inside_lab_section = False  # Stop ignoring text
 
         # Detect lab results section
         for y in labPhrases:
             if re.search(y, line, re.IGNORECASE):
                 inside_lab_section = True  
-                new_text.append("*lab results*")  # redact added
+                new_text.append("*lab results*\n")  # redact added
                 break # skip the line
                 
         # append only lines not inside the lab results section
