524109fb114e70cbc9464b94eb2c453595138f1d
RonanRao <164988959+RonanRao@users.noreply.github.com>
2025-04-10 22:23:42 -0500

    Update ip_address.py
    returns a tuple, also updated the ipv6 regex so it is more accurate

--- Regexs/ip_address.py
+++ Regexs/ip_address.py
@@ -4,6 +4,9 @@ import re
 IP_TOKEN = '*ip address*'
 
 def remove_ipaddress(text):
-    ip_finder = re.compile(r"\b(((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]).){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])) | ([0-9a-z]{4}:){7}[0-9a-z]{4}\b")
-    return ip_finder.sub(IP_TOKEN, text)
+    ip_finder = re.compile(r"\b(?:(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)|(?:[0-9a-f]{1,4}:){7}[0-9a-f]{1,4})\b", re.IGNORECASE)
+    
+    found_ip = ip_finder.findall(text)
+    replaced_text = ip_finder.sub(IP_TOKEN, text)
+    return (replaced_text, found_ip)
     
