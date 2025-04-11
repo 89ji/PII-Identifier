569d8fef4acd5778c5cfb5244cdae727d6366a04
RonanRao <164988959+RonanRao@users.noreply.github.com>
2025-04-10 22:17:25 -0500

    Update deviceidentifiers.py
    now returns a tuple

--- Regexs/deviceidentifiers.py
+++ Regexs/deviceidentifiers.py
@@ -4,7 +4,11 @@ import re
 def remove_device_identifiers(text):
     DI_TOKEN = "*device identifiers*"
 
-    di_finder = re.compile(r"\b[A-Z0-9]{6}-[A-Z0-9]{8}\b | ([A-Z0-9]{2}-){5}[A-Z0-9]{2}\b", re.IGNORECASE)
-    return di_finder.sub(DI_TOKEN,text)
+    di_finder = re.compile(r"\b([A-Z0-9]{6}-[A-Z0-9]{8}|\b(?:[A-Z0-9]{2}-){5}[A-Z0-9]{2})\b", re.IGNORECASE)
+    
+    found_di = di_finder.findall(text)
+    replaced_text = di_finder.sub(DI_TOKEN,text)
+    return (replaced_text, found_di)
+
 
  
