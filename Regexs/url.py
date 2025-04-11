4e01dd80a0a04fc83cc29820d17c25abc0233d28
RonanRao <164988959+RonanRao@users.noreply.github.com>
2025-04-10 22:21:02 -0500

    Update url.py
    now returns a tuple

--- Regexs/url.py
+++ Regexs/url.py
@@ -4,6 +4,10 @@ import re
 URL_TOKEN = '*url*'
 
 def remove_urls(text):
-    url_finder = re.compile(r"\b(www.)([a-z0-9]+)(.com|.org|.net|.edu|.gov|.mil|.io)\b")
-    return url_finder.sub(URL_TOKEN, text)
+    url_finder = re.compile(r"\b(www\.[a-z0-9-]+\.(?:com|org|net|edu|gov|mil|io))\b", re.IGNORECASE)
+    
+    found_url = url_finder.findall(text)
+    replaced_text = url_finder.sub(URL_TOKEN, text)
+    return (replaced_text, found_url)
+    
     
